f4005ae43071684e50848cf5245e71f197e48114
nbroeder <nbroeder@uwo.ca>
2022-11-19 12:58:17 -0500

    Turn text into array of tiles

--- color/app.js
+++ color/app.js
@@ -44,6 +44,32 @@ let interpretColor = (colour) => {
     }
 }
 
+let emojiToColor = (colour) => {
+    const upperCaseColor = colour.toUpperCase();
+    switch(upperCaseColor) {
+        case "RED":
+            return ":red_square:";
+        case "ORANGE":
+            return ":orange_square:";
+        case "YELLOW":
+            return ":yellow_square:";
+        case "GREEN":
+            return ":green_square:";
+        case "BLUE":
+            return ":blue_square:";
+        case "PURPLE":
+            return ":purple_square:";
+        case "BROWN":
+            return ":brown_square:";
+        case "BLACK":
+            return ":black_large_square:";
+        case "WHITE":
+            return ":white_large_square:";
+        default:
+            return ":white_large_square:";
+    }
+}
+
 let formatTileLink = (x, y, colour) => {
     return "https://github.com/lemos00/git-place/issues/new?title=newcolour%7C" + x + "%7C" + y + "%7C" + colour + "&body=Please+Input+your+color+and+%27Submit+new+issue%27"
 }
@@ -52,15 +78,23 @@ let formatIndividualTile = (colour, link) => {
     return "[" + interpretColor(colour) + "]" + "(" + link + ")";
 }
 
+let toTileArray = (gridText)=> {
+    let array = gridText.split("<!---->");
+
+    let tileArray = []
+    for(let i =0;i<array.length;i++){
+        let tempTile = new Tile(i, 0, array[i]);
+        tileArray.push(tempTile);
+    }
+    return tileArray;
+}
+
+
 ///////main\\\\\\\\
 
 let newTile = getTile();
 let text = fs.readFileSync("./grid.txt", "utf8");
-let textGrid = text.split("<!---->");
-
-console.log(textGrid);
-
-//let location = newTile.Y*WIDTH + newTile.X;
+let textGrid = toTileArray(text);
 
 textGrid[newTile.X] = interpretColor(newTile.colour);
 
