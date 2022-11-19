ec4e301a896cc5154e2f98043d2e68e69b0df466
Gabriel Lemos Rodrigues <glemos.rodrigues@gmail.com>
2022-11-19 17:39:50 -0500

    add space to readme

--- color/app.js
+++ color/app.js
@@ -153,8 +153,8 @@ let writeMultipleRows = (tileArray) => {
     let newReadmeRows = ""
 
     // add title and description of what to do
-    newReadmeRows += "#git-place\n"
-    newReadmeRows += "###To submit your color, click on a pixel, and swap 'WHITE' on the title with your color of choice.\n"
+    newReadmeRows += "# git-place\n"
+    newReadmeRows += "### To submit your color, click on a pixel, and swap 'WHITE' on the title with your color of choice.\n"
 
     for (let i = 0; i < tileArray.length; i++) {
         newReadmeRows += writeToReadme(tileArray[i]);
