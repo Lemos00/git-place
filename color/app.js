e89ef9586cdf88522d9ca6235026b312e5fe813d
nbroeder <nbroeder@uwo.ca>
2022-11-19 11:04:00 -0500

    Added a grid database to save the state of the grid

--- color/app.js
+++ color/app.js
@@ -1,12 +1,43 @@
 const fs = require("fs");
 const core = require("@actions/core");
+const width = 37;
+const height = 5;
 
-let getColour = () =>{
-    let issueTitle = core.getInput('title');
-    let titleSplit = issueTitle.split("|");
-    return titleSplit[1]
+function Tile(colour, Xpos, Ypos){
+    this.colour = colour,
+    this.X = Xpos,
+    this.Y = Ypos
 }
 
-let text = fs.readFileSync("./test.txt", "utf8");
-text+=getColour(); 
-fs.writeFileSync("./test.txt", text + "\n" + "this is new text");
+
+let getTile = () =>{
+    let issueTitle = core.getInput('title');
+    let titleSplit = issueTitle.split("|");
+    let tile = Tile(titleSplit[1], titleSplit[2], titleSplit[3]);
+    return tile;
+}
+
+///////main\\\\\\\\
+
+newTile = getTile();
+
+
+let text = fs.readFileSync("./grid.txt", "utf8");
+
+
+textArray = text.split("<!---->");
+let location = newTile.Y*width + newTile.X;
+
+textArray[location] = "NEW TILE";
+
+let newGrid = "";
+let i =0;
+for(e in textArray){
+    newGrid+=e;
+    newGrid+="<!---->";
+    if(i%37 == 0)
+        newGrid+="\n";
+    i++;
+}
+
+fs.writeFileSync("./grid.txt", newGrid);
