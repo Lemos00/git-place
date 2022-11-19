5806b728306326a67cccc221fcdd309f295332ee
Gabriel Lemos Rodrigues <glemos.rodrigues@gmail.com>
2022-11-19 01:46:13 -0500

    remove server temporarily

--- color/app.js
+++ color/app.js
@@ -1,20 +1,22 @@
+// // const fs = require("fs");
+// const http = require("http");
 // const fs = require("fs");
-const http = require("http");
-const fs = require("fs");
-const port = 3000;
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.write("Hello Node");
-    res.end();
-    let text = fs.readFileSync("notes.txt", "utf-8");
-    console.log(text);
-})
+// const server = http.createServer((req, res) => {
+//     res.write("Hello Node");
+//     res.end();
+//     let text = fs.readFileSync("notes.txt", "utf-8");
+//     console.log(text);
+// })
 
 
-server.listen(port, (error) => {
-    if (error) {
-        console.log("something went wrong");
-    } else {
-        console.log("Server is listening on port " + port);
-    }
-})
+// server.listen(port, (error) => {
+//     if (error) {
+//         console.log("something went wrong");
+//     } else {
+//         console.log("Server is listening on port " + port);
+//     }
+// })
+
+console.log("yo")
