b2a3369129d15ceef8fb2564231c4f0cec25c5ef
Gabriel Lemos Rodrigues <glemos.rodrigues@gmail.com>
2022-11-19 01:55:54 -0500

    move test.txt

--- color/app.js
+++ color/app.js
@@ -20,5 +20,5 @@ const fs = require("fs");
 // })
 
 console.log("yo")
-const text = fs.readFileSync("./test.txt", "utf-8");
+const text = fs.readFileSync("./test.txt", "utf8");
 fs.writeFileSync("./test.txt", text);
